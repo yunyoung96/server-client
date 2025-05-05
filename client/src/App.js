3d0ea3857f15d4c237b39747b7e4be5c19287951
yunyoung96 <yunyoungchoi96@gmail.com>
2025-05-05 14:37:39 +0900

    fix the client App.js to receive data from server

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [str, setStr] = useState("");
+
+  useEffect(() => {
+    fetch('https://server-of-server-client.vercel.app/')
+      .then((res) => res.json())
+      .then((data) => {
+        setStr(data.message);
+      })
+      .catch((err) => {
+        console.error('Error fetching message:', err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>{str}</div>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.use("/", (req, res) => {
     res.send("Server is running.");
+
+    res.status(200).json({ message: 'haha' });
 });
 
 app.listen(3000, console.log("Server started on PORT 3000"));
